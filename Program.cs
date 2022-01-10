26985fa0d9c3417b1546f326f147317861cb3ce5
Olga <ove2501@mail.ru>
2022-01-10 21:25:23 +0500

    add fibonacci1

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿double Fibonacci (int n)//было инт, искали для 10. дабл для 40
+{
+    if(n == 1|| n== 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);//обязательно смотри - должно быть вернуть
+}
+
+for (int i = 1; i< 40; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
